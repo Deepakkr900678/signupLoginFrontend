da75e9b6c9a38d5a70ae42b879b0dfc488a9c5cb
Deepak Kumar <deepak.groobizz@gmail.com>
2024-03-06 09:03:25 +0530

    added redux function

---
+++ src/components/LoginForm.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+export default function LoginForm() {
+    const [formData, setFormData] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        confimPassword: "",
+    })
+    return (
+        <>
+            
+        </>
+    )
+}
